dacbd908d300e7789bf83006805ed5f50c395527
Peekar <85645128+Peekar@users.noreply.github.com>
2021-06-15 17:36:17 -0500

    new notes

---
+++ stringSlicing.py
@@ -0,0 +1,22 @@
+school = "kilLIAn"
+name = "sRikAr KOthA"
+# We want to get the first letter upper and the rest lower in Killian, to make it a proper noun
+
+print(school[0]) # This will show us the first character in the list, starting from 0
+# (if you go over the number of characters in the string, there will be a out of range error)
+
+length = len(school) # creating a variable for the length of the school
+print(length)
+print(length-2)
+
+# print(variable[<start (characters start at 0) dft-0>:<end dft-last (length start at 1) character>:<increment dft-1>]) dft is default
+print(school[2:7:])
+print(school[2::2])
+print(school[5:1:-1])
+
+print(school[0].upper() + school[1:].lower()) # Using everything, we have solved it!!!
+
+# We can also use .upper(), .lower(), and .capitalize can do things some things as well
+print(school.upper())
+print(school.lower())
+print(school.capitalize())
