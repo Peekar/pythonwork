2b1c303e13b37d34adcd1d40a623436d84203d9a
Peekar <85645128+Peekar@users.noreply.github.com>
2021-06-21 16:33:57 -0500

    new file

---
+++ Homework.py
@@ -0,0 +1,7 @@
+# You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+# ex: Input - I am a person, Output - I-am-a-person
+s = input()
+
+s = s.split(" ")
+s = "-".join(s)
+print(s)
