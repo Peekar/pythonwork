5968a9f23accd112c0dc66d1c70eeb025949c0b4
Peekar <85645128+Peekar@users.noreply.github.com>
2021-07-01 17:13:58 -0500

    take a string and return it reversed

--- Homework.py
+++ Homework.py
@@ -1,30 +1,11 @@
-from typing import List
+string = str(input())
+length = len(string)
+output = []
+proceed = input()
 
-iterations = int(input("How many commands do you want to enter?: "))
-print("")
-list = []
-iterationNum = 0
+while proceed == 'y':
+    for i in range(length):
+        output.append(string[length - 1])
+        length = length - 1
 
-for i in range(iterations):
-    iterationNum = iterationNum + 1
-    commandInput = input(f"What command do you want to enter? (command {iterationNum}/{iterations}): ")
-    command = commandInput.split()
-    commandWord = command[0]
-
-    if  commandWord == 'insert':
-        index = int(command[2])
-        list.insert(index, command[1])
-    elif commandWord == 'print':
-        print(*list)
-    elif commandWord == 'remove':
-        list.remove(command[1])
-    elif commandWord == 'append':
-        list.append(command[1])
-    elif commandWord == 'sort':
-        list.sort()
-    elif commandWord == 'pop':
-        list.pop()
-    elif commandWord == 'reverse':
-        list.reverse()
-    else:
-        print("Something went wrong, please try again.")
+print(*output, sep="")
